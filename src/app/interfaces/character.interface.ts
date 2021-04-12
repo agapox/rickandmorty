78c2d1e905c42e802664f8de81e9080c7ea800fd
jramirez <jose.ramirez@siigroup.cl>
2021-04-12 12:31:32 -0400

    FIX: adding expected data of interface

--- src/app/interfaces/character.interface.ts
+++ src/app/interfaces/character.interface.ts
@@ -2,7 +2,21 @@ export interface Character {
   id:	number; //	The id of the character.
   name:	string;	// The name of the character.
   status:	string; //	The status of the character ('Alive', 'Dead' or 'unknown').
-  species:	string; //	The species of the character.
+  species:	string;
+    /*
+      The species of the character.
+      0: "Human"
+      1: "Alien"
+      2: "Humanoid"
+      3: "unknown"
+      4: "Poopybutthole"
+      5: "Mythological Creature"
+      6: "Animal"
+      7: "Robot"
+      8: "Cronenberg"
+      9: "Disease"
+      10: "Planet"
+    */
   type:	string;	// The type or subspecies of the character.
   gender:	string; //	The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
   origin:	{
