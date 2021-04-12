0f03a50114a02e46aac4fc03a44365eb4c419954
jramirez <jose.ramirez@siigroup.cl>
2021-04-12 16:39:50 -0400

    FIX: Refactor app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,38 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Observable, of, throwError } from 'rxjs';
-import { delay } from 'rxjs/operators';
-import { GetCharacterHttpResMock, GetCharactersHttpResMock } from 'src/assets/mocks/character.mock';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { CharacterService } from './services/character.service';
-import { EpisodeService } from './services/episode.service';
-import { LocationService } from './services/location.service';
+
 
 describe('AppComponent', () => {
-  let characterService: CharacterService;
-  let locationService: LocationService;
-  let episodeService: EpisodeService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HttpClientModule
-      ],
       declarations: [
         AppComponent
-      ],
-      providers: [
-        CharacterService,
-        LocationService,
-        EpisodeService
       ]
     }).compileComponents();
-
-    characterService = TestBed.inject(CharacterService);
-    locationService = TestBed.inject(LocationService);
-    episodeService = TestBed.inject(EpisodeService);
 
   });
 
@@ -40,37 +17,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-    expect(characterService).toBeTruthy();
-    expect(locationService).toBeTruthy();
-    expect(episodeService).toBeTruthy();
-  });
-
-  it(`should have as title 'Rick and Morty'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Rick and Morty');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.main-title').textContent).toContain(fixture.componentInstance.title);
-  });
-
-  it('should call characterService.getCharacters and save pages', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    spyOn(characterService, 'getCharacters').and.callFake((page) => {
-      return of(GetCharactersHttpResMock)
-    });
-    let pages = 0;
-    characterService.getCharacters(1).subscribe(data => {
-      pages = data.info.pages;
-    })
-    app.ngOnInit();
-    expect(pages).toEqual(34);
-    expect(app.getCharacterPages).toBeTruthy()
   });
 
 });
