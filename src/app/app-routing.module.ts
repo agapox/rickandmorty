6334fb00ae9c18ea32229a50464730b0f83502f1
jramirez <jose.ramirez@siigroup.cl>
2021-04-12 12:29:31 -0400

    FIX: refactor of home to show router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
